6aff29ad361d18ddd7a7ea8814dc22c08f7d1648
mzbasler <basler.mauricio@gmail.com>
2022-11-19 19:24:23 -0300

    classe MEC e Principal finalizadas

--- src/MEC.java
+++ src/MEC.java
@@ -2,13 +2,18 @@ public class MEC {
 
     public static void imprimeUniversidades(Universidade[] arrayUniversidade) {
         System.out.println();
-        System.out.println    ("----------IMPRIME UNIVERSIDADES----------");
+        System.out.println("-----------------------------------------");
+        System.out.println("UNIVERSIDADES CADASTRADAS");
+        System.out.println("-----------------------------------------");
+
+        // FOR QUE PERCORRE O ARRAYLIST E IMPRIME AS INFORMAÇÕES DAS UNIVERSIDADES
         for (int i = 0; i < arrayUniversidade.length; i++) {
-            System.out.println("Universisdade "+(i+1));
             System.out.println("-----------------------------------------");
             arrayUniversidade[i].imprimeInfo();
+            // SE É UMA INSTANCIA DE PRIVADA ELE IMPRIME INICIATIVA PRIVADA
             if (arrayUniversidade[i] instanceof Privada)
                 System.out.println("Iniciativa: Privada");
+            // SE É UMA INSTANCIA DE PRIVADA ELE IMPRIME INICIATIVA PUBLICA
             if (arrayUniversidade[i] instanceof Publica)
                 System.out.println("Iniciativa: Pública");
             System.out.println("-----------------------------------------");
@@ -18,14 +23,23 @@ public class MEC {
     public static void maisCara(Universidade[] arrayUniversidade) {
         int index = 0;
         double universidadeMaisCara = 0;
+        // FOR QUE PERCORRE O ARRAYLIST
         for (int i = 0; i < arrayUniversidade.length; i++) {
+            // VERIFICA SE É UMA INSTANCIA DA CLASSE PRIVADA
             if (arrayUniversidade[i] instanceof Privada) {
-                universidadeMaisCara = ((Privada) arrayUniversidade[i]).getValorMensalidade();
-                index = i;
+                // VERIFICA SE VARIÁVEL É MENOR QUE O VALOR DA MENSALIDADE INDEXADA
+                if (universidadeMaisCara < ((Privada) arrayUniversidade[i]).getValorMensalidade()) {
+                    // VARIÁVEL RECEBE VALOR DA MENSALIDADE SE ESTE FOR MENOR QUE A MENSALIDADE DA POSIÇÃO DO ÍNDICE
+                    universidadeMaisCara = ((Privada) arrayUniversidade[i]).getValorMensalidade();
+                    // RECEBE O VALOR DO ÍNDICE PARA IMPRIMIR AS INFOS DA UNIVERSIDADE EM QUESTÃO.
+                    index = i;
+                }
             }
         }
         System.out.println();
-        System.out.println("---------UNIVERSIDADE MAIS CARA----------");
+        System.out.println("-----------------------------------------");
+        System.out.println("UNIVERSIDADE PRIVADA MAIS CARA");
+        System.out.println("-----------------------------------------");
         ((Privada) arrayUniversidade[index]).imprimeInfo();
         System.out.println("-----------------------------------------");
     }
@@ -33,12 +47,18 @@ public class MEC {
     public static void universidadesDoSul(Universidade[] arrayUniversidade) {
         String aux;
         System.out.println();
-        System.out.println("----------UNIVERSIDADES DO SUL-----------");
+        System.out.println("-----------------------------------------");
+        System.out.println("UNIVERSIDADES PUBLICAS DO SUL");
+        System.out.println("-----------------------------------------");
+        // FOR QUE PERCORRE ARRAYLIST
         for (int i = 0; i < arrayUniversidade.length; i++) {
+            // VERIFICA SE É UMA INSTANCIA DA CLASSE PUBLICA E AUX RECEBE O ESTADO
             if (arrayUniversidade[i] instanceof Publica) {
                 aux = (((Publica) arrayUniversidade[i]).getEstado());
-                if(aux.equals("RS") || aux.equals("SC") || aux.equals("PR")) {
+                // VERIFICA SE VARIAVEL AUX É IGUAL À RS, SC OU PR.
+                if(aux.equalsIgnoreCase("RS") || aux.equalsIgnoreCase("SC") || aux.equalsIgnoreCase("PR")) {
                     System.out.println("-----------------------------------------");
+                    //IMPRIME INFORMAÇÕES DO ARRAYLIST
                     ((Publica) arrayUniversidade[i]).imprimeInfo();
                     System.out.println("-----------------------------------------");
                 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,37 +1,67 @@
 public class Principal {
+    public static void main(String[] args) {
 
-    public static void main(String[] args){
+        int tamanhoArray = Teclado.leInt("Digite o número de Universidades que você deseja cadastrar: ");
+        Universidade[] arrayUniversidade = new Universidade[tamanhoArray];
 
-        int tamanhoArray = Teclado.leInt("Digite o numero de universidades que você deseja cadastrar: ");
-        Universidade [] arrayUniversidade = new Universidade[tamanhoArray];
+        for (int i = 0; i < arrayUniversidade.length; i++) {
+            System.out.println();
+            System.out.println("Que tipo de Universidade você deseja cadastrar? ");
+            System.out.println("[ 1 ]- Pública");
+            System.out.println("[ 2 ]- Privada");
+            System.out.println();
+            int publicaOuPrivada = Teclado.leInt("Digite uma numero: ");
+            System.out.println();
 
-
-        for (int i = 0; i < arrayUniversidade.length; i++){
-
-            System.out.println("Que tipo de universidade você deseja incluir? ");
-            int publicaOuPrivada = Teclado.leInt("Digite uma opção: 1 - Pública | 2 - Privada: ");
-
-            if(publicaOuPrivada == 1)
+            if (publicaOuPrivada == 1) {
                 arrayUniversidade[i] = new Publica(
-                    Teclado.leString("Insira o nome da Universidade: "),
-                    Teclado.leInt("Insira a quantidade de alunos: "),
-                    Teclado.leInt("Insira a quantidade de Professores: "),
-                    Teclado.leString("Insira o Estado: "),
-                    Teclado.leString("Insira a Cidade: ")
+                        Teclado.leString("Insira o nome da Universidade: "),
+                        Teclado.leInt("Insira a quantidade de alunos: "),
+                        Teclado.leInt("Insira a quantidade de Professores: "),
+                        Teclado.leString("Insira o Estado: "),
+                        Teclado.leString("Insira a Cidade: ")
                 );
-
-            if( publicaOuPrivada == 2)
+                System.out.println("(*) Universidade cadastrada com sucesso!");
+            }
+            if (publicaOuPrivada == 2) {
                 arrayUniversidade[i] = new Privada(
-                    Teclado.leString("Insira o nome da Universidade: "),
-                    Teclado.leInt("Insira a quantidade de alunos: "),
-                    Teclado.leInt("Insira a quantidade de Professores: "),
-                    Teclado.leDouble("Insira o valor da mensalidade: ")
+                        Teclado.leString("Insira o nome da Universidade: "),
+                        Teclado.leInt("Insira a quantidade de alunos: "),
+                        Teclado.leInt("Insira a quantidade de Professores: "),
+                        Teclado.leDouble("Insira o valor da mensalidade: ")
                 );
+                System.out.println("(*) Universidade cadastrada com sucesso!");
+            }
         }
 
-        MEC.imprimeUniversidades(arrayUniversidade);
-        //MEC.maisCara(arrayUniversidade);
-        MEC.universidadesDoSul(arrayUniversidade);
+        boolean aux = true;
+        while(aux){
+            System.out.println();
+            System.out.println("------------------MENU-------------------");
+            System.out.println("[ 1 ] - Imprimir universidades");
+            System.out.println("[ 2 ] - Imprimir universidade mais cara");
+            System.out.println("[ 3 ] - Imprimir universidades do Sul");
+            System.out.println("[ 4 ] - Sair");
+            System.out.println("-----------------------------------------");
 
+            int sCase = Teclado.leInt("Digite um numero: ");
+
+            switch(sCase) {
+                case 1:
+                    MEC.imprimeUniversidades(arrayUniversidade);
+                    break;
+                case 2:
+                    MEC.maisCara(arrayUniversidade);
+                    break;
+                case 3:
+                    MEC.universidadesDoSul(arrayUniversidade);
+                    break;
+                case 4:
+                    System.out.println("(*) Logout realizado com sucesso!");
+                    System.exit(0);
+                    break;
+                default:
+            }
+        }
     }
 }
